4021fac7101344e142a704e4aaae1d562c84d96f
BARJ215 <32267193+BARJ215@users.noreply.github.com>
2018-02-12 15:35:33 +0000

    android

--- www/js/index.js
+++ www/js/index.js
@@ -11,11 +11,6 @@ function initMap(){
 function detectBrowser() {
     var useragent = navigator.userAgent;
     var mapdiv = document.getElementById("map");
-    if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
-        mapdiv.style.width = '100%';
-        mapdiv.style.height = '100%';
-    } else {
-        mapdiv.style.width = '600px';
-        mapdiv.style.height = '800px';
-    }
+    mapdiv.style.width = '600px';
+    mapdiv.style.height = '800px';
 }

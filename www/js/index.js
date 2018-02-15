729d701f7aeec5b2164c7523329f7a7bd9c20a89
BARJ215 <32267193+BARJ215@users.noreply.github.com>
2018-02-15 17:59:24 +0000

    dadadsd

--- www/js/index.js
+++ www/js/index.js
@@ -18,35 +18,43 @@ $(document).on('pageinit', function(){
 
 //Call this function when you want to get the current position
 function getPosition() {
+	console.log("getting data");
+	//change time box to show updated message
+	//$('#time').val("Getting data...");
+	
 	//instruct location service to get position with appropriate callbacks
 	navigator.geolocation.getCurrentPosition(successPosition, failPosition);
 }
+
+
 //called when the position is successfully determined
 function successPosition(position) {
 	
 	//You can find out more details about what the position obejct contains here:
 	// http://www.w3schools.com/html/html5_geolocation.asp
 	
+
 	//lets get some stuff out of the position object
-	// var unixTime = new Date(position.timestamp);
-    // var date = unixTime.toDateString();
+	var unixTime = new Date(position.timestamp);
+    var date = unixTime.toDateString();
 	var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
-    // var acc = position.coords.accuracy;
-    // var alt = position.coords.altitude;
-    // var altAcc = position.coords.altitudeAccuracy;
-    // var head = position.coords.heading;
-    // var speed = position.coords.speed;
-    currentCenter.lat=latitude;
-    currentCenter.lng=longitude;
-    console.log(longitude,latitude);
-
+    var acc = position.coords.accuracy;
+    var alt = position.coords.altitude;
+    var altAcc = position.coords.altitudeAccuracy;
+    var head = position.coords.heading;
+    var speed = position.coords.speed;
+    
+    console.log(latitude);
+	
 }
 
 //called if the position is not obtained correctly
 function failPosition(error) {
 	//change time box to show updated message
-    console.log("ERROR: "+ error);
+    //$('#time').val("Error getting data: " + error);
+    console.log("Error getting data:"+error);
+	
 }
 	
 function initMap(){
